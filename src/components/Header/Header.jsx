631c97d1e4be82be108837e9d331ab8dadddbf46
manish257 <manishmahato257@gmail.com>
2022-07-06 16:33:41 +0500

    styling of header social media

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Header.css';
+import CTA from './CTA';
+import ME from '../../assets/me3.png';
+import HeaderSocials from './HeaderSocials';
 
 function Header() {
   return (
@@ -14,6 +17,13 @@ function Header() {
           <h5 className='text-light'>
             Fullstack Developer
           </h5>
+          <CTA />
+          <HeaderSocials />
+          <div className='me'>
+            <img src={ME} alt="me" />
+          </div>
+
+          <a href='#contact' className='scroll__down'>Scroll Down</a>
         </div>
     </header>
   )
