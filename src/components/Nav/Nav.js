3422a202c8d0e495c441393e78c21bc2268d8241
manish257 <manishmahato257@gmail.com>
2022-07-06 17:28:18 +0500

    links to the navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Nav.css';
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineUser} from 'react-icons/ai';
+import {BiBook} from 'react-icons/bi';
+import {RiServiceLine} from 'react-icons/ri';
+import {BiMessageSquareDetail} from 'react-icons/bi';
 
 function Nav() {
   return (
     <nav>
-        <a href="#"></a>
+        <a href="#"><AiOutlineHome /></a>
+        <a href="#about"><AiOutlineUser /></a>
+        <a href="#experience"><BiBook /></a>
+        <a href="#services"><RiServiceLine /></a>
+        <a href="#contact"><BiMessageSquareDetail /></a>
     </nav>
   )
 }
