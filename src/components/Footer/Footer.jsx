9b31cfdb55b9af781afba397dff0708a6b07f3e0
manish257 <manishmahato257@gmail.com>
2022-07-08 11:58:57 +0500

    start of footer jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Footer.css';
+import {BsInstagram} from 'react-icons/bs';
+import {ImFacebook} from 'react-icons/im';
+import {RiTwitterFill} from 'react-icons/ri';
+
+
+function Footer() {
+  return (
+    <footer>
+      <a href='#' className='footer__logo'>Manish</a>
+      <ul className='permalinks'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a> 
+        </li>
+        <li>
+          <a href='#services'>Services</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#testimonials'>Testimonials</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href='https://www.instagram.com/manishkusw/' target="_blank"
+                  rel="noreferrer"><BsInstagram /></a>
+        <a href='https://www.facebook.com/portrayalss/' target="_blank"
+                  rel="noreferrer"><ImFacebook /></a>
+        <a href='https://twitter.com/portrayals143' target="_blank"
+                  rel="noreferrer"><RiTwitterFill /></a>
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer;
